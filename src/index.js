019625e10afc308ad0aab433c65bdb2feb6e3776
Miranda a.k.a. Scout <160192777+scoutdoesthings@users.noreply.github.com>
2024-06-30 09:10:29 -0400

    Update index.js
    removed unnecessary comments to make it easier to read. changed setDelay to set difficulty

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 const startButton = document.querySelector('#start');
-const score = document.querySelector("#score"); // Use querySelector() to get the score element
-const timerDisplay = document.querySelector("#timer"); // use querySelector() to get the timer element.
+const score = document.querySelector('#score'); // Use querySelector() to get the score element
+const timerDisplay = document.querySelector('#timer'); // use querySelector() to get the timer element.
 
 let time = 0;
 let timer;
@@ -12,12 +12,6 @@ let difficulty = "hard";
 
 /**
  * Generates a random integer within a range.
- *
- * The function takes two values as parameters that limits the range 
- * of the number to be generated. For example, calling randomInteger(0,10)
- * will return a random integer between 0 and 10. Calling randomInteger(10,200)
- * will return a random integer between 10 and 200.
- *
  */
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -25,20 +19,9 @@ function randomInteger(min, max) {
 
 /**
  * Sets the time delay given a difficulty parameter.
- *
- * The function takes a `difficulty` parameter that can have three values: `easy`
- * `normal` or `hard`. If difficulty is "easy" then the function returns a time delay
- * of 1500 milliseconds (or 1.5 seconds). If the difficulty is set to "normal" it should
- * return 1000. If difficulty is set to "hard" it should return a randomInteger between
- * 600 and 1200.
- *
- * Example: 
- * setDelay("easy") //> returns 1500
- * setDelay("normal") //> returns 1000
- * setDelay("hard") //> returns 856 (returns a random number between 600 and 1200).
- *
  */
 function setDelay(difficulty) {
+
   if (difficulty === "easy") {
     return 1500;
   } else if (difficulty === "normal") {
@@ -50,17 +33,6 @@ function setDelay(difficulty) {
 
 /**
  * Chooses a random hole from a list of holes.
- *
- * This function should select a random Hole from the list of holes.
- * 1. generate a random integer from 0 to 8 and assign it to an index variable
- * 2. get a random hole with the random index (e.g. const hole = holes[index])
- * 3. if hole === lastHole then call chooseHole(holes) again.
- * 4. if hole is not the same as the lastHole then keep track of 
- * it (lastHole = hole) and return the hole
- *
- * Example: 
- * const holes = document.querySelectorAll('.hole');
- * chooseHole(holes) //> returns one of the 9 holes that you defined
  */
 function chooseHole(holes) {
   let hole;
@@ -74,24 +46,7 @@ function chooseHole(holes) {
 }
 
 /**
-*
 * Calls the showUp function if time > 0 and stops the game if time = 0.
-*
-* The purpose of this function is simply to determine if the game should
-* continue or stop. The game continues if there is still time `if(time > 0)`.
-* If there is still time then `showUp()` needs to be called again so that
-* it sets a different delay and a different hole. If there is no more time
-* then it should call the `stopGame()` function. The function also needs to
-* return the timeoutId if the game continues or the string "game stopped"
-* if the game is over.
-*
-*  // if time > 0:
-*  //   timeoutId = showUp()
-*  //   return timeoutId
-*  // else
-*  //   gameStopped = stopGame()
-*  //   return gameStopped
-*
 */
 function gameOver() {
   if(time > 0){
@@ -103,17 +58,11 @@ function gameOver() {
 }
 }
 /**
-*
 * Calls the showAndHide() function with a specific delay and a hole.
-*
-* This function simply calls the `showAndHide` function with a specific
-* delay and hole. The function needs to call `setDelay()` and `chooseHole()`
-* to call `showAndHide(hole, delay)`.
-*
 */
 function showUp() {
-  let delay = setDelay('easy'); // TODO: Update so that it uses setDelay()
-  const hole = chooseHole(holes);  // TODO: Update so that it use chooseHole()
+  let delay = setDelay('difficulty');
+  const hole = chooseHole(holes); 
   return showAndHide(hole, delay);
 }
 
@@ -164,6 +113,7 @@ function updateScore() {
   score.textContent = points;
   return points;
 }
+updateScore();
 
 /**
 *
